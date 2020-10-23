0f3d457557dca188129019257cd5db5307e507f3
Arissetiawan1 <50401597+Arissetiawan1@users.noreply.github.com>
2020-10-23 11:11:51 +0700

    Update course.php

--- code/menuoption/course.php
+++ code/menuoption/course.php
@@ -14,18 +14,29 @@
 <section id="features" class="features"><div class="container">
 
 <div class="row" data-aos="fade-up" data-aos-delay="300">
-  <div class="col-lg-3 col-sm-4" >
+  <div class="col-lg-4 col-lg-4" >
     <div class="icon-box">
       <i class="ri-store-line" style="color: #ffbb2c;"></i>
       <h4><a href="">Assessment</a></h4>
     </div>
   </div>
+    
+    <div class="col-lg-3 col-sm-4">
+    <div class="icon-box">
+      <i class="ri-store-line" style="color: #ffbb2c;"></i>
+      <h3><a href="">Assesment</a></h3>
+    </div>
+  </div>
+    
   <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
     <div class="icon-box">
       <i class="ri-bar-chart-box-line" style="color: #5578ff;"></i>
       <h4><a href="">Business and operations</a></h4>
     </div>
   </div>
+    
+    
+    
   <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
     <div class="icon-box">
       <i class="ri-calendar-todo-line" style="color: #e80368;"></i>
